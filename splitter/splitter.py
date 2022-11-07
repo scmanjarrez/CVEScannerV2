e5efa53cd5c2f63f546da210573586185ac553fa
scmanjarrez <serchiman@gmail.com>
2022-11-07 11:20:16 +0100

    Fixed incorrect LICENSE

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -20,7 +20,7 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from pathlib import Path
 
